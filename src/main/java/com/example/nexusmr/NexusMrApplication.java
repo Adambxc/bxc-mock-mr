c33c32483136eab4cb02cf569dec1832973946cb
Adam Sedgwick <robert.sedgwick@bettercloud.com>
2023-02-16 16:20:38 -0500

    init

---
+++ src/main/java/com/example/nexusmr/NexusMrApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexusmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusMrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NexusMrApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/nexusmr/NexusMrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexusmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusMrApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

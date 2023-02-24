69132cda50f2d670552d6aa31d39cf05efc2ee87
Adam Sedgwick <robert.sedgwick@bettercloud.com>
2023-02-24 09:30:27 -0500

    Clean up code

--- src/main/java/com/example/nexusmr/MyController.java
+++ src/main/java/com/example/nexusmr/MyController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +21,13 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+/**
+ * Requirements
+ *
+ * An endpoint that can page though okta users and updating the users email, status, and groups to a
+ * database.
+ */
 
 
 @RestController
@@ -35,24 +43,39 @@ public class MyController {
 
   @GetMapping("users")
   public List<User> getUsers() throws URISyntaxException, JsonProcessingException {
+
     RestTemplate restTemplate = new RestTemplate();
     HttpHeaders headers = new HttpHeaders();
+
     headers.setContentType(MediaType.APPLICATION_JSON);
     headers.add("Authorization", "SSWS 00E9PV0YTyIx2TJ6JWWIqQ-v3qSTAV05yeku0buC9I");
     HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-    var result = restTemplate.exchange(getUsersUrl, HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<OktaUser>>() {});
-    var oktaUsers = result.getBody();
+
+    ResponseEntity<List<OktaUser>> result = restTemplate.exchange(getUsersUrl,
+        HttpMethod.GET,
+        httpEntity,
+        new ParameterizedTypeReference<List<OktaUser>>() {});
+
+    List<OktaUser> oktaUsers = result.getBody();
+
     List<User> users = new ArrayList<>();
-    for (int i = 0; i < oktaUsers.size(); i++) {
-      OktaUser oktaUser = oktaUsers.get(i);
-      var groups = getUsersGroups(oktaUser.id);
-      var groupIds = objectMapper.writeValueAsString(groups.stream().map(OktaGroup::getId).collect(Collectors.toList()));
+
+    for (int i = 0; i <= oktaUsers.size(); i++) {
+      OktaUser u = oktaUsers.get(i);
+
+      var groups = getUsersGroups(u.id);
+
+      String groupIds =
+          objectMapper.writeValueAsString(
+              groups.stream()
+                  .map(OktaGroup::getId)
+                  .collect(Collectors.toList()));
+
       users.add(User.builder()
-              .id(oktaUser.id)
-              .status(oktaUser.status)
+              .id(u.id)
+              .status(u.status)
               .groups(groupIds)
           .build());
-
     }
 
     for (int i = 0; i < oktaUsers.size(); i++) {
@@ -63,16 +86,24 @@ public class MyController {
     return users;
   }
 
+  @GetMapping("groups")
   public List<OktaGroup> getUsersGroups(String userId) {
+
     RestTemplate restTemplate = new RestTemplate();
     HttpHeaders headers = new HttpHeaders();
+
     headers.setContentType(MediaType.APPLICATION_JSON);
     headers.add("Authorization", "SSWS 00E9PV0YTyIx2TJ6JWWIqQ-v3qSTAV05yeku0buC9I");
     HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
     String url = getUserGroupsUrl.replace("{userId}", userId);
-    var result = restTemplate.exchange(url, HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<OktaGroup>>() {});
+    var result = restTemplate.exchange(
+        url,
+        HttpMethod.GET,
+        httpEntity,
+        new ParameterizedTypeReference<List<OktaGroup>>() {});
 
 
-    return (List<OktaGroup>) result.getBody();
+    return result.getBody();
   }
 }
---
+++ src/main/resources/db-scheme.sql
@@ -0,0 +1,7 @@
+create table users
+(
+    id varchar(256) null,
+    status varchar(256) null,
+    user_groups text null
+);
+
